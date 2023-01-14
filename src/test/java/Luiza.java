696b60be3035a8aa08828fe28863f599820b3468
luizakurbanova <luizakurbanovaa01@gmail.com>
2023-01-13 19:26:25 -0600

    'Luiza's message'

--- src/test/java/Luiza.java
+++ src/test/java/Luiza.java
@@ -1,2 +1,11 @@
 public class Luiza {
+
+    public static void main(String[] args) {
+        String message= "raeY weN yppaH";
+
+        for (int i=message.length()-1; i>=0; i--){
+            System.out.print(message.charAt(i));
+        }
+    }
+
 }
